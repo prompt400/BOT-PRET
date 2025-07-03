342e84819921bedc8384efab28be0243ced081a7
prompt400 <prompt400@gmail.com>
2025-07-03 20:22:41 +0200

    clean: remove all old files

--- src/types/Event.ts
+++
@@ -1,8 +0,0 @@
-import { ClientEvents } from 'discord.js';
-import { BotClient } from '../structures/BotClient.js';
-
-export interface Event<K extends keyof ClientEvents = keyof ClientEvents> {
-  name: K;
-  once?: boolean;
-  execute: (client: BotClient, ...args: ClientEvents[K]) => void | Promise<void>;
-}
